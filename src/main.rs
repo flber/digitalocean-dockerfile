4cbeef18c4ed8a9b521379de413577e51f6245c6
flbr <benrhammond@gmail.com>
2023-02-24 13:50:20 -0800

    adds say hi button!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use rocket::fs::FileServer;
+use rocket::response::content::RawJson;
+use rocket::State;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+struct UserCount(AtomicUsize);
+
+#[get("/count")]
+fn count(user_count: &State<UserCount>) -> RawJson<String> {
+    RawJson(format!(
+        "{{\"user_count\": {}}}",
+        user_count.0.load(Ordering::Relaxed)
+    ))
+}
+
+#[post("/count")]
+fn inc_count(user_count: &State<UserCount>) {
+	user_count.0.fetch_add(1, Ordering::SeqCst);
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", FileServer::from("public/"))
+        .manage(UserCount(AtomicUsize::new(0)))
+        .mount("/", FileServer::from("public/"))
+        .mount("/api", routes![count, inc_count])
 }
