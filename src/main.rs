a00e8937157cfc72e69e0e972851d6c2384b8881
flbr <benrhammond@gmail.com>
2023-02-23 20:55:45 -0800

    changes port to 8080 for DO app

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", routes![index])
 }
---
+++ target/debug/deps/cmsrs-56e835ac01aed56f.d
@@ -0,0 +1,5 @@
+/home/benh/projects/digitalocean-dockerfile/target/debug/deps/cmsrs-56e835ac01aed56f.rmeta: src/main.rs
+
+/home/benh/projects/digitalocean-dockerfile/target/debug/deps/cmsrs-56e835ac01aed56f.d: src/main.rs
+
+src/main.rs:
