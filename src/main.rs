b7674f134f7f01923bc93c18614e6653866d1674
flbr <benrhammond@gmail.com>
2023-02-24 00:42:34 -0800

    updates main.rs to be fileserver, adds style to public/

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 #[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "omg does this fully work?!"
-}
+use rocket::fs::FileServer;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
+    .mount("/", FileServer::from("public/"))
 }
