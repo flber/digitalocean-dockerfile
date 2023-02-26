8c79b8246c65fe8d330fb2b792ffd08042f1b1ca
flbr <benrhammond@gmail.com>
2023-02-25 21:22:00 -0800

    adds pillar tooling

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::response::content::RawJson;
 use rocket::State;
+use std::net::{SocketAddr, /*IpAddr*/};
 
+// use std::sync::Mutex;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 struct UserCount(AtomicUsize);
+// struct UserIPs(Vec<Mutex<IpAddr>>);
 
 #[get("/count")]
 fn count(user_count: &State<UserCount>) -> RawJson<String> {
@@ -17,14 +20,16 @@ fn count(user_count: &State<UserCount>) -> RawJson<String> {
 }
 
 #[post("/count")]
-fn inc_count(user_count: &State<UserCount>) {
-	user_count.0.fetch_add(1, Ordering::SeqCst);
+fn inc_count(user_count: &State<UserCount>, sock: SocketAddr) {
+    user_count.0.fetch_add(1, Ordering::SeqCst);
+    println!("Remote Address: {:?}", sock.ip());
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(UserCount(AtomicUsize::new(0)))
+        // .manage(UserIPs(vec![]))
         .mount("/", FileServer::from("public/"))
         .mount("/api", routes![count, inc_count])
 }
